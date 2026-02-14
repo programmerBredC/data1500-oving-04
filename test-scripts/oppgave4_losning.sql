1c1bdcfdcf0f152d40c2bf6bb74406d4bf8a56c1
christianbredli <christiaaa@hotmail.com>
2026-02-14 18:41:07 +0100

    Add oppgave4_losning.sql

--- test-scripts/oppgave4_losning.sql
+++ test-scripts/oppgave4_losning.sql
@@ -6,3 +6,25 @@
 
 -- En test SQL-spørring mot metadata i PostgreSQL
 select nspname as schema_name from pg_catalog.pg_namespace;
+
+SELECT * FROM innlegg WHERE avsender IN (
+        SELECT bruker_id FROM lærere
+        )
+    AND rom_id = 1 ORDER BY dato DESC LIMIT 3;
+
+WITH RECURSIVE traad AS (
+  SELECT * 
+  FROM innlegg
+  WHERE avsender = 2 AND opprinnelig_innlegg IS NULL
+
+  UNION ALL
+
+  SELECT i.*
+  FROM innlegg i
+  JOIN traad t ON i.opprinnelig_innlegg = t.innleggs_id
+)
+SELECT * FROM traad; 
+
+SELECT * FROM elev_i_gruppe WHERE gruppe_id = 1
+
+SELECT COUNT(gruppe_id) FROM grupper;
